3351327ee742b553c507f45f80945ac5b6b7d5e7
wraithwood <d.mccallister1757@gmail.com>
2025-02-01 23:47:37 -0500

    Add simple turn-based mechanic and AI

--- maps/maps - Copy/scripts/characters/soldier_ai.gd
+++ maps/maps - Copy/scripts/characters/soldier_ai.gd
@@ -1,39 +1,30 @@
 extends Sprite2D
 
-@onready var animation_player = $AnimationPlayer
-@onready var pathfinding_manager = get_parent().get_node("PathfindingManager")
+@onready var player = get_parent().get_node("Goblin")
+@onready var path_manager = get_parent().get_node("PathfindingManager")
 @onready var tile_map_layer: TileMapLayer = get_parent().get_node("TileLayers/Pathfinding")
-@onready var turn_manager: Node2D = get_parent().get_node("TurnManager")
+@onready var turn_manager = get_parent().get_node("TurnManager")
 var is_moving: bool = false
 var current_path: Array = []
-var has_action: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	animation_player.get_animation("idle").loop = true
-	animation_player.play("idle")
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print("Is Goblin's turn? ", turn_manager.is_my_turn("goblin"))
-	if not is_moving and turn_manager.is_my_turn("goblin"):
-		_on_player_input(delta)
+	if not is_moving and turn_manager.is_my_turn("soldier"):
+		_move_toward_player(delta)
 		
-	_update_movement(delta)
-				
-func _on_player_input(delta: float) -> void:
-	if Input.is_action_just_pressed("MoveLeft") and not is_moving:
-		_move_sprite_left(delta)
-	if Input.is_action_just_pressed("MoveRight") and not is_moving:
-		_move_sprite_right(delta)
-	if Input.is_action_just_pressed("MoveUp") and not is_moving:
-		_move_sprite_up(delta)
-	if Input.is_action_just_pressed("MoveDown") and not is_moving:
-		_move_sprite_down(delta)
-		
+	_update_movement(delta)	
+	
+func _move_toward_player(delta: float) -> void:
+	_move_sprite(delta, _current_position(), _player_current_position())
+	
 func _move_sprite(delta: float, current_position: Vector2i, destination: Vector2i) -> void:
-	current_path = pathfinding_manager.find_path(current_position, destination)
+	current_path = path_manager.find_path(current_position, destination)
+	current_path = current_path.slice(0, 4)
 	if not current_path.is_empty():
 		is_moving = true
 			
@@ -45,7 +36,8 @@ func _move_sprite(delta: float, current_position: Vector2i, destination: Vector2
 			current_path.remove_at(0)
 			if current_path.is_empty():
 				is_moving = false
-				turn_manager.turn_is_complete("goblin")
+				turn_manager.turn_is_complete("soldier")
+				
 				
 func _move_sprite_left(delta: float) -> void:
 	var current_position = _current_position()
@@ -69,14 +61,17 @@ func _move_sprite_down(delta: float) -> void:
 
 func _update_movement(delta: float) -> void:
 	if is_moving and not current_path.is_empty():
-		var pixel_position_destination = tile_map_layer.map_to_local(current_path[0]) + pathfinding_manager.tile_size() / 2
-		position = position.move_toward(pixel_position_destination, delta * 200)
+		var pixel_position_destination = tile_map_layer.map_to_local(current_path[0]) + path_manager.tile_size() / 2
+		position = position.move_toward(pixel_position_destination, delta * 100)
 		
 		if position.distance_to(pixel_position_destination) < 1:
 			current_path.remove_at(0)
 			if current_path.is_empty():
 				is_moving = false
-				turn_manager.turn_is_complete("goblin")
+				turn_manager.turn_is_complete("soldier")
 				
 func _current_position() -> Vector2:
-	return tile_map_layer.local_to_map(global_position - pathfinding_manager.tile_size() / 2)
+	return tile_map_layer.local_to_map(global_position - path_manager.tile_size() / 2)
+	
+func _player_current_position() -> Vector2:
+	return tile_map_layer.local_to_map(player.global_position - path_manager.tile_size() / 2)
